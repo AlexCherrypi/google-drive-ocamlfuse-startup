a444d43faf61745635f6018dee7780550a586280
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-12-28 00:32:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,6 @@ chmod 0755 google-drive-ocamlfuse-startup.sh && \
 sudo mkdir -p /etc/systemd/user/ && \
 cd /etc/systemd/user/ && \
 sudo wget https://raw.githubusercontent.com/Knallbertlp/google-drive-ocamlfuse-startup/master/google-drive-ocamlfuse-startup.service -O google-drive-ocamlfuse-startup.service && \
-chmod 0644 google-drive-ocamlfuse-startup.service
+chmod 0644 google-drive-ocamlfuse-startup.service && \
+sudo enable google-drive-ocamlfuse-startup.service && \
+sudo start google-drive-ocamlfuse-startup.service

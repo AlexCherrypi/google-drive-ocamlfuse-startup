ecb13ed5a927786fb74ca33b6549c5560f0e0f07
KnallbertLp <KnallbertLp@users.noreply.github.com>
2020-12-28 01:33:42 +0100

    Update google-drive-ocamlfuse-startup.sh

--- google-drive-ocamlfuse-startup.sh
+++ google-drive-ocamlfuse-startup.sh
@@ -14,7 +14,7 @@ for file in ~/.gdfuse/*/  ; do
   local LABEL
   LABEL="$(basename $file)"
   local DIR
-  DIR="~/Google\ Drive/Google\ Drive\ $LABEL"
+  DIR="~/Google Drive/Google Drive $LABEL"
   local SERVICE
   SERVICE="google-drive-ocamlfuse-instance-$LABEL.service"
   unset file
